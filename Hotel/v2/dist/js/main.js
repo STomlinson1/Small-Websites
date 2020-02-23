ae8cf29bebb060f7e76ab913900652a215975509
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-02-23 01:59:00 -0500

    finished v2 home

---
+++ Hotel/v2/dist/js/main.js
@@ -0,0 +1,22 @@
+const nav = document.createElement('div');
+nav.innerHTML = `
+<h1 class="nav__logo">HBT</h1>
+<ul class="nav__links">
+  <li><a class="nav__links__link" href="index.html">Home</a></li>
+  <li><a class="nav__links__link" href="about.html">About</a></li>
+  <li><a class="nav__links__link" href="contact.html">Contact</a></li>
+</ul>
+`;
+
+const navParent = document.querySelector('.nav__parent');
+navParent.appendChild(nav);
+
+const footer = document.createElement('div');
+footer.innerHTML = `
+<footer class="footer">
+<p>Hotel BT &copy; 2020, All Rights Reserved</p>
+</footer>
+`;
+
+const footerParent = document.querySelector('.footer__parent');
+footerParent.appendChild(footer);
